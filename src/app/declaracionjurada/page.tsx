894b9295f8966816819aa9a40b0bc4b8adda9956
gerar10 <gerarburgos1987@gmail.com>
2023-05-21 13:36:59 -0300

    WIP declaracionjurada

---
+++ src/app/declaracionjurada/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function declaracionJurada() {
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+    Declaración Jurada
+  </h1>
+  <h2> ¿Ha consumido bebidas alcoholicas en las ultimas 12 horas?</h2>
+    <div>
+  <button type="button" className="text-black bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
+  <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
+  </div>
+  </>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import React from "react";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
