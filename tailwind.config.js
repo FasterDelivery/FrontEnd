730dc11c62617268f39ffae8b94774ef16477187
Alexis <edbwinn@gmail.com>
2023-05-26 14:01:38 -0300

    tailwind color settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,18 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+      },
+      colors:{
+        "dark-blue-button": "#217BCE",
+        "cyan text": "#217BCE",
+        "yellow-label-text": "#FEBC14",
+        "gray-paragraphs":"#4F4F4F",
+        "yellow-button-weekly":"#FCBC11",
+        "20%":"#6373F7",
+        "60%": "#FCBC11",
+        "80%": "#FEBD93",
+        "100%":"#96DB76",
+        "black-opaque-50%":"#00000080"
       }
     }
   },
