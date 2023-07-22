a12ba7686841bc2a894cc5c5e7ce9112bc09ca67
gerar10 <gerarburgos1987@gmail.com>
2023-07-22 20:58:27 -0300

    wipredux

--- src/app/delivery/mapa.tsx
+++ src/app/delivery/mapa.tsx
@@ -6,6 +6,7 @@ import {
   DirectionsRenderer
 } from "@react-google-maps/api";
 import { useState, useEffect, ReactElement } from "react";
+import Spinner from "../Components/spinner";
 
 const mapOptions = {
   disableDefaultUI: true,
@@ -89,9 +90,7 @@ const Mapa = ({
           )}
         </GoogleMap>
       ) : (
-        <div className="flex justify-center pt-40 h-screen">
-          <div className="border-t-8 border-orange-500 rounded-full animate-spin ease-linear w-12 h-12"></div>
-        </div>
+        <Spinner />
       )}
     </>
   );
