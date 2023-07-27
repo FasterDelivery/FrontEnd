23e71104832546fd2fe78c825cbb40ac2d32054c
MateoCata98 <mate.catalano98@gmail.com>
2023-07-27 16:43:52 -0300

    fix Add Package

--- src/app/addpackages/page.tsx
+++ src/app/addpackages/page.tsx
@@ -3,21 +3,69 @@ import React, { FormEvent } from "react";
 import { BackButton, Navbar } from "app/Components";
 import useInput from "../hooks/useInput";
 import useControllCountPackages from "../hooks/useControllCountPackages";
+import axios from "axios";
+import { headers } from "next/dist/client/components/headers";
+import Swal from "sweetalert2";
 
 const Page = () => {
-  const address = useInput(),
-    name = useInput(),
-    kg = useInput(),
-    date = useInput(),
-    controllCountPackages = useControllCountPackages();
+  const clientname = useInput();
+  const controllCountPackages = useControllCountPackages();
+  const weight = useInput();
+  const day: any = useInput();
+  const street = useInput();
+  const number = useInput();
+  const city = useInput();
+  const province = useInput();
+  const postalCode = useInput();
+  const deliDay = new Date(day.value);
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
-    console.log(address.value);
-    console.log(name.value);
-    console.log(kg.value);
-    console.log(date.value);
-    console.log(controllCountPackages.state);
+    const body = {
+      clientname: clientname.value,
+      image: "http://dummyimage.com/218x100.png/5fa2dd/ffffff",
+      quantity: controllCountPackages.state,
+      weight: weight.value,
+      deliveryday: deliDay,
+      street: street.value,
+      number: number.value,
+      city: city.value,
+      province: province.value,
+      postalCode: postalCode.value
+    };
+    axios
+      .post("https://3.91.204.112/api/packages/new", body, {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwYXlsb2FkIjp7ImVtYWlsIjoiYWRtaW5AYWRtaW4uY29tIiwicGFzc3dvcmQiOiIkMmIkMDkkZy4xS3ZyTWpiam11NkNKd2N1Rk5FZW4uSmh1cHpGV25lZUFRejAuWVBpcHNDTDdvVTIuaTIiLCJpc0FkbWluIjp0cnVlfSwiaWF0IjoxNjkwNDg1MjcwLCJleHAiOjE2OTA0OTI0NzB9.EW_BODtDF85-cpE-m7J9uP3dZu8Lzk4ZuYCPR2ranhw"
+        }
+      })
+      .then((res) => {
+        console.log(res);
+        Swal.fire({
+          title: "OK",
+          text: `Paquete creado correctamente`,
+          icon: "info",
+          confirmButtonText: "Continuar",
+          confirmButtonColor: "#217BCE",
+          customClass: {
+            popup: "sm:w-1/2"
+          }
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        return Swal.fire({
+          title: "Errores al crear paquete.",
+          text: `Por favor, intente nuevamente.`,
+          icon: "warning",
+          confirmButtonText: "Continuar",
+          confirmButtonColor: "#217BCE",
+          customClass: {
+            popup: "sm:w-1/2"
+          }
+        });
+      });
   };
 
   return (
@@ -34,15 +82,59 @@ const Page = () => {
           </h1>
         </div>
         <form onSubmit={handleSubmit}>
-          <div className="w-90 mx-auto" style={{ marginBottom: "15px" }}>
-            <h1 className="text-md text-yellow-400">Direccion</h1>
-            <input
-              type="text"
-              id="address"
-              className="border-b-2 border-blue-500 focus:outline-none w-full"
-              {...address}
-              required
-            />
+          <div className="w-90 mx-auto flex" style={{ marginBottom: "15px" }}>
+            <div className="w-50 pr-2">
+              <h1 className="text-md text-yellow-400">Ciudad</h1>
+              <input
+                type="text"
+                id="city"
+                className="border-b-2 border-blue-500 focus:outline-none w-full"
+                {...city}
+                required
+              />
+            </div>
+            <div className="w-50 pl-2">
+              <h1 className="text-md text-yellow-400">Provincia</h1>
+              <input
+                type="text"
+                id="province"
+                className="border-b-2 border-blue-500 focus:outline-none w-full"
+                {...province}
+                required
+              />
+            </div>
+          </div>
+          <div className="w-90 mx-auto flex" style={{ marginBottom: "15px" }}>
+            <div className="w-50 pr-1">
+              <h1 className="text-md text-yellow-400">Direccion</h1>
+              <input
+                type="text"
+                id="street"
+                className="border-b-2 border-blue-500 focus:outline-none w-full"
+                {...street}
+                required
+              />
+            </div>
+            <div className="w-2/4 pl-2">
+              <h1 className="text-md text-yellow-400">Nro</h1>
+              <input
+                type="number"
+                id="number"
+                className="border-b-2 border-blue-500 focus:outline-none w-full"
+                {...number}
+                required
+              />
+            </div>
+            <div className="w-1/4 pl-2">
+              <h1 className="text-md text-yellow-400">CP</h1>
+              <input
+                type="text"
+                id="postalCode"
+                className="border-b-2 border-blue-500 focus:outline-none w-full"
+                {...postalCode}
+                required
+              />
+            </div>
           </div>
 
           <div className="w-90 mx-auto" style={{ marginBottom: "15px" }}>
@@ -51,7 +143,7 @@ const Page = () => {
               type="text"
               id="recipient-name"
               className="border-b-2 border-blue-500 focus:outline-none w-full"
-              {...name}
+              {...clientname}
               required
             />
           </div>
@@ -59,10 +151,10 @@ const Page = () => {
           <div className="w-90 mx-auto" style={{ marginBottom: "15px" }}>
             <h1 className="text-md text-yellow-400">Peso (Kg)</h1>
             <input
-              type="text"
+              type="number"
               id="weight"
               className="border-b-2 border-blue-500 focus:outline-none w-full"
-              {...kg}
+              {...weight}
               required
             />
           </div>
@@ -71,10 +163,10 @@ const Page = () => {
               Fecha en la que debe ser repartido
             </h1>
             <input
-              type="text"
+              type="date"
               id="date"
               className="border-b-2 border-blue-500 focus:outline-none w-full"
-              {...date}
+              {...day}
               required
             />
           </div>
